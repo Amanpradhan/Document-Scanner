328ea78442e4b57bac6edf56acfe8211d3b14da9
Amanpradhan <aprdhn.30000@gmail.com>
2018-12-02 23:41:42 +0530

    updated scan.py

--- scan.py
+++ scan.py
@@ -188,7 +188,7 @@ class DocScanner():
             plt.imshow(rescaled_image)  
             plt.show()  
 
-            
+                
         # also attempt to find contours directly from the edged image, which occasionally 
         # produces better results
         (_, cnts, hierarchy) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -272,35 +272,9 @@ class DocScanner():
 
 
 if __name__ == "__main__":
-    ap = argparse.ArgumentParser()
-    # group = ap.add_mutually_exclusive_group(required=True)
-    # group.add_argument("--images", help="Directory of images to be scanned")
-    # group.add_argument("--image", help="Path to single image to be scanned")
-    ap.add_argument("-i", action='store_true',
-        help = "Flag for manually verifying and/or setting document corners")
-
-    args = vars(ap.parse_args())
-    # im_dir = args["images"]
-    # im_file_path = args["image"]
-    # interactive_mode = args["i"]
-
-    # scanner = DocScanner(interactive_mode)
+    
     scanner = DocScanner()
-
-    valid_formats = [".jpg", ".jpeg", ".jp2", ".png", ".bmp", ".tiff", ".tif"]
-
-    get_ext = lambda f: os.path.splitext(f)[1].lower()
-
-    im_file_path = 'test_s1.jpg'
-
+    im_file_path = input()
     scanner.scan(im_file_path)
 
-    # Scan single image specified by command line argument --image <IMAGE_PATH>
-    # if im_file_path:
-    #     scanner.scan(im_file_path)
-
-    # Scan all valid images in directory specified by command line argument --images <IMAGE_DIR>
-    # else:
-    #     im_files = [f for f in os.listdir(im_dir) if get_ext(f) in valid_formats]
-    #     for im in im_files:
-    #         scanner.scan(im_dir + '/' + im)
+    
